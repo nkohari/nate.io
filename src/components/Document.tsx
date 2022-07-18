0d54079e5132f3d80fa67635a5dcc5a9c7f1d9ea
Nate Kohari <nkohari@gmail.com>
2022-07-18 09:26:53 -0400

    Fix hash links and footnotes

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,7 +1,22 @@
+import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 export type DocumentProps = {
   children: React.ReactNode;
 };
 
-export const Document = ({ children }: DocumentProps) => (
-  <article className="flex-1 w-full">{children}</article>
-);
+export const Document = ({ children }: DocumentProps) => {
+  const location = useLocation();
+
+  // If the URL contains a hash which refers to a DOM element on the page, scroll to it.
+  // This check needs to happen inside Document to ensure the content is loaded before
+  // trying to check for the DOM element.
+  useLayoutEffect(() => {
+    if (location.hash?.length > 1) {
+      const el = document.getElementById(location.hash.substring(1));
+      el?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [location]);
+
+  return <article className="flex-1 w-full">{children}</article>;
+};

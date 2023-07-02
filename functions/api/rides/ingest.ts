b08cae07a434faddcc2120c180dbcdf4eec7e82b
Nate Kohari <nkohari@gmail.com>
2023-07-02 15:44:32 -0400

    Return errors

--- functions/api/rides/ingest.ts
+++ functions/api/rides/ingest.ts
@@ -28,7 +28,7 @@ async function getStravaAccessToken(env: Env) {
 
   const {access_token, refresh_token} = await response.json<any>();
   if (refresh_token !== refreshToken) {
-    env.HEALTH.put(STRAVA_REFRESH_TOKEN_KEY, refresh_token);
+    await env.HEALTH.put(STRAVA_REFRESH_TOKEN_KEY, refresh_token);
   }
 
   return access_token;
@@ -57,30 +57,28 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   const {env, request} = context;
   const url = new URL(request.url);
 
-  if (request.method === 'GET') {
-    const token = url.searchParams.get('hub.verify_token');
-    const challenge = url.searchParams.get('hub.challenge');
+  try {
+    if (request.method === 'GET') {
+      const token = url.searchParams.get('hub.verify_token');
+      const challenge = url.searchParams.get('hub.challenge');
 
-    console.log('[challenge] %o', {token, challenge});
-
-    if (challenge && token === env.STRAVA_VERIFY_TOKEN) {
-      console.log('[challenge] matched');
-      return createJsonResponse({'hub.challenge': challenge});
+      if (challenge && token === env.STRAVA_VERIFY_TOKEN) {
+        return createJsonResponse({'hub.challenge': challenge});
+      }
     }
-  }
 
-  if (request.method === 'POST') {
-    const {aspect_type, object_id, object_type} = await request.json<any>();
-    console.log('Got webhook %o', {aspect_type, object_id, object_type});
+    if (request.method === 'POST') {
+      const {aspect_type, object_id, object_type} = await request.json<any>();
 
-    if (aspect_type === 'create' && object_type === 'activity') {
-      const ride = await getRideFromStrava(env, object_id);
-      console.log('Got ride %o', ride);
-      const json = JSON.stringify(ride, null, 2);
-      env.HEALTH.put(`ride:${ride.id}`, json);
-      console.log('[ride created] %o', json);
-      return createJsonResponse(json);
+      if (aspect_type === 'create' && object_type === 'activity') {
+        const ride = await getRideFromStrava(env, object_id);
+        const json = JSON.stringify(ride, null, 2);
+        env.HEALTH.put(`ride:${ride.id}`, json);
+        return createJsonResponse(json);
+      }
     }
+  } catch (error) {
+    return new Response(JSON.stringify({error}), {status: 500});
   }
 
   return new Response('Bad request', {status: 400});

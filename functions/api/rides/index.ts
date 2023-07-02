78ee2a1d46e9caef982f6476f57929ec687e937d
Nate Kohari <nkohari@gmail.com>
2023-07-02 14:20:03 -0400

    Add console.logs

--- functions/api/rides/index.ts
+++ functions/api/rides/index.ts
@@ -10,16 +10,22 @@ async function getAllRides(env: Env): Promise<Ride[]> {
   let cursor = undefined;
   let result: KVNamespaceListResult<never, string>;
 
+  console.log('Loading rides from KV');
+
   do {
     result = await env.HEALTH.list({cursor, prefix: 'ride:'});
     keys = [...keys, ...result.keys.map((key) => key.name)];
   } while (!result.list_complete);
+
+  console.log('Got ride keys: %o', keys);
 
   let rides = [];
   for (const key of keys) {
     const ride = await env.HEALTH.get(key, {type: 'json'});
     rides.push(ride);
   }
+
+  console.log('Loaded rides');
 
   return rides;
 }

125424de70e9e3b81160edbb46aeceea7d13b211
Nate Kohari <nkohari@gmail.com>
2023-08-17 08:47:41 -0400

    Little link effect

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -15,13 +15,13 @@ export const LINK_CLASSES: Record<LinkType, string> = {
   block:
     'transition transition-colors bg-slate-200 dark:bg-slate-600 hover:bg-blue-600 dark:hover:bg-blue-400 hover:text-white',
   normal:
-    'transition transition-all duration-200 underline underline-offset-1 text-slate-700 decoration-slate-400 dark:text-slate-300 dark:decoration-slate-500 hover:text-blue-600 hover:dark:text-blue-300 hover:decoration-blue-600 hover:dark:decoration-blue-300',
+    'transition transition-all duration-200 underline underline-offset-2 hover:underline-offset-4 text-slate-700 decoration-slate-400 dark:text-slate-300 dark:decoration-slate-500 hover:text-blue-600 hover:dark:text-blue-300 hover:decoration-blue-600 hover:dark:decoration-blue-300',
   spotify:
-    'transition transition-all duration-200 hover:underline underline-offset-1 decoration-transparent hover:text-[#1db954] hover:decoration-[#1db954] dark:hover:text-[#1db954] dark:hover:decoration-[#1db954]',
+    'transition transition-all duration-200 hover:underline underline-offset-2 hover:underline-offset-4 decoration-transparent hover:text-[#1db954] hover:decoration-[#1db954] dark:hover:text-[#1db954] dark:hover:decoration-[#1db954]',
   subtle:
-    'transition transition-all duration-200 hover:underline underline-offset-1 decoration-transparent hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400',
+    'transition transition-all duration-200 hover:underline underline-offset-2 decoration-transparent hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400',
   'subtle-with-underline':
-    'transition transition-all duration-200 underline underline-offset-1 decoration-slate-400 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400',
+    'transition transition-all duration-200 underline underline-offset-2 decoration-slate-400 hover:text-blue-600 hover:decoration-blue-600 dark:hover:text-blue-400 dark:hover:decoration-blue-400',
 };
 
 export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {

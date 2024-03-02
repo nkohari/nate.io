6dfb36181712bb353b9e08d27e752122bb36c17c
Nate Kohari <nkohari@gmail.com>
2024-03-02 08:13:56 -0500

    Rename KV binding

--- functions/api/rides/index.ts
+++ functions/api/rides/index.ts
@@ -2,11 +2,11 @@ import {createJsonResponse} from '../../common/createJsonResponse';
 import {Ride} from '../../../src/types';
 
 type Env = {
-  HEALTH: KVNamespace;
+  CACHE: KVNamespace;
 };
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   const {env} = context;
-  const rides = await env.HEALTH.get<Ride[]>('rides', {type: 'json'});
+  const rides = await env.CACHE.get<Ride[]>('rides', {type: 'json'});
   return createJsonResponse(rides);
 };
--- functions/api/rides/ingest.ts
+++ functions/api/rides/ingest.ts
@@ -4,14 +4,14 @@ import {Ride} from '../../../src/types';
 const STRAVA_REFRESH_TOKEN_KEY = 'strava-refresh-token';
 
 type Env = {
-  HEALTH: KVNamespace;
+  CACHE: KVNamespace;
   STRAVA_CLIENT_ID: string;
   STRAVA_CLIENT_SECRET: string;
   STRAVA_VERIFY_TOKEN: string;
 };
 
 async function getStravaAccessToken(env: Env) {
-  const refreshToken = await env.HEALTH.get(STRAVA_REFRESH_TOKEN_KEY);
+  const refreshToken = await env.CACHE.get(STRAVA_REFRESH_TOKEN_KEY);
 
   const response = await fetch('https://www.strava.com/api/v3/oauth/token', {
     method: 'POST',
@@ -28,7 +28,7 @@ async function getStravaAccessToken(env: Env) {
 
   const {access_token, refresh_token} = await response.json<any>();
   if (refresh_token !== refreshToken) {
-    await env.HEALTH.put(STRAVA_REFRESH_TOKEN_KEY, refresh_token);
+    await env.CACHE.put(STRAVA_REFRESH_TOKEN_KEY, refresh_token);
   }
 
   return access_token;
@@ -56,24 +56,27 @@ async function getRideFromStrava(env: Env, id: string): Promise<Ride> {
 
 async function saveRideToKV(env: Env, ride: Ride): Promise<Ride[]> {
   // Read the existing values from KV.
-  const existingRides = await env.HEALTH.get<Ride[]>('rides', {type: 'json'});
+  const existingRides = await env.CACHE.get<Ride[]>('rides', {type: 'json'});
 
   // Create a hash from the existing rides to ensure that we don't inadvertently save duplicates.
-  const hash = existingRides.reduce((result, ride) => {
-    result[ride.id] = ride;
-    return result;
-  }, {} as {[id: string]: Ride});
+  const hash = existingRides.reduce(
+    (result, ride) => {
+      result[ride.id] = ride;
+      return result;
+    },
+    {} as {[id: string]: Ride},
+  );
 
   // Add the ride to the hash.
   hash[ride.id] = ride;
 
   // Sort the values of the hash by timestamp.
   const rides = Object.values(hash).sort(
-    (a, b) => Date.parse(a.timestamp) - Date.parse(b.timestamp)
+    (a, b) => Date.parse(a.timestamp) - Date.parse(b.timestamp),
   );
 
   // Write the values back to KV.
-  await env.HEALTH.put('rides', JSON.stringify(rides));
+  await env.CACHE.put('rides', JSON.stringify(rides));
 
   return rides;
 }

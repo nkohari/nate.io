d70508a0af16a75f872fe18653195f4c81abd4b5
Nate Kohari <nkohari@gmail.com>
2024-05-14 18:23:20 -0400

    Move to markdoc 0.4 (apocrypha 0.2.1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', 'content/**/*.md', 'src/**/*.{ts,tsx}'],
-  darkMode: 'class',
+  darkMode: 'selector',
   theme: {
     fontFamily: {
       sans: "'SN Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
